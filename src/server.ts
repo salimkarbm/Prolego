00c42435e84b706a9492d0f72f68031095146598
salim imuzai <salimimuzai@gmail.com>
2022-05-17 15:37:27 +0100

    add eslint and prettier - add eslint and prettier for code formatting - ensure eslint and prettier are correctly working [Delivers #Eslint & Prettier]

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,24 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from 'express';
 
-//Initialize express
-const app: express.Application = express()
+// Initialize express
+const app: express.Application = express();
 
-//Port
-const address = '0.0.0.0:8080'
-const PORT = process.env.PORT || 8080
+// Port
+const address = '0.0.0.0:8080';
+const PORT = process.env.PORT || 8080;
 
-//Body parser middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-//Defines index routes
+// Define index route
 app.get('/', async (req: Request, res: Response) => {
   res.send(
     "Welcome to Prolego. The following API's are available to be accessed:"
-  )
-})
+  );
+});
 
-//Listen for server connections
-app.listen(PORT, () => console.log(`server running on ${address}`))
+// Listen for server connections
+app.listen(PORT, () => console.log(`server running on ${address}`));
 
-export default app
+export default app;
